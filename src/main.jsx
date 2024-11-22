3abaa54355f5205d2f1dd181e51898a3247a28cd
garen <karmartir@gmail.com>
2024-11-22 14:49:13 -0500

    less 44 Data fetching via loader()

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Posts from "./routes/Posts";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Posts, {loader as postsLoader} from './routes/Posts';
 import NewPost from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
 
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
+        loader: postsLoader,
         children: [{ path: "/create-post", element: <NewPost /> }],
       },
       { path: "/create-post", element: <NewPost /> },

ff88b77f019ee79f9141a6e431396e27472adf4c
garen <karmartir@gmail.com>
2024-11-22 14:32:44 -0500

    less 44 Linking & Navigating

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import classes from './Modal.module.css'
-const Modal = ({children, onClose}) => {
+const Modal = ({children}) => {
+
+const navigate = useNavigate()
+
+function closeHandler(){
+ //you can use '/' or this '..' which means go up to parent route
+  navigate('..')
+}
+
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose}/>
+      <div className={classes.backdrop} onClick={closeHandler}/>
         <dialog open className={classes.modal}>{children}</dialog>
     </>
   );
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import Modal from "../components/Modal";
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 
-function NewPost({ onCancel, onAddPost }) {
+function NewPost({ onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -37,9 +38,9 @@ function NewPost({ onCancel, onAddPost }) {
         <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
-        <button type="button" onClick={onCancel}>
+        <Link to='..' type="button">
           Cancel
-        </button>
+        </Link>
         <button>Submit</button>
       </p>
     </form>
